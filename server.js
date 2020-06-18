5fe1e0310779fbe425d33a1c21f98ab3a84f921c
ThenmozhiGit <thenmozhi.cse@gmail.com>
2020-06-18 08:01:06 +0530

    Set up routes for CRUD

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.get('/api/v1/crud', (req, res) => {
 	res.status(200).json({ success: true, msg: `Show all api` });
 });
 
+app.post('/api/v1/crud', (req, res) => {
+	res.status(200).json({ success: true, msg: `Create new` });
+});
+
+app.put('/api/v1/crud/:id', (req, res) => {
+	res.status(200).json({ success: true, msg: `Update ${req.params.id}` });
+});
+
+app.delete('/api/v1/crud/:id', (req, res) => {
+	res.status(200).json({ success: true, msg: `Delete ${req.params.id}` });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
