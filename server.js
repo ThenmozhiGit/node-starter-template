a1008d11f23f175f5f935b8165d413fc4fa50d52
ThenmozhiGit <thenmozhi.cse@gmail.com>
2020-06-19 21:13:30 +0530

    Configured morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 //Route files
 const customRouter = require('./routes/router');
 //Load env vars
@@ -8,8 +8,10 @@ dotenv.config({ path: './config/.env' });
 
 const app = express();
 
-//Logger
-app.use(logger);
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('default'));
+}
 //Mount routers
 app.use('/api/v1', customRouter);
 
